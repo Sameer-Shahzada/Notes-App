1a869eb5230cea3661d5c523180921ee14fc914d
Shahzada <md.shahzada1606@gmail.com>
2022-07-24 22:14:33 +0530

    create table in entity room database

--- app/src/main/java/com/example/notes/Note.java
+++ app/src/main/java/com/example/notes/Note.java
@@ -1,6 +1,19 @@
 package com.example.notes;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+// define room entity with new table name by tableName property
+@Entity(tableName = "notes_table")
+
 public class Note {
-    // Note class code here
+
+    @PrimaryKey(autoGenerate = true)
+    int id = 0;
+
+    @ColumnInfo(name = "text")
+    public String text;
+
 
 }
