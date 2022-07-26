30161d397376b0a41a3dfb5b6284915ae591ae0c
Shahzada <md.shahzada1606@gmail.com>
2022-07-26 19:02:42 +0530

    create Note Room Database

--- app/src/main/java/com/example/notes/NoteDao.java
+++ app/src/main/java/com/example/notes/NoteDao.java
@@ -1,5 +1,6 @@
 package com.example.notes;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -20,7 +21,7 @@ public interface NoteDao {
     void delete(Note note);     // to delete note
 
     @Query("Select * from notes_table order by id ASC")
-    List<Note> getAllNotes();       // to get all notes.
+    LiveData<List<Note>> getAllNotes();       // to get all notes.
 
 
 }
